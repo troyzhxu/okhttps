d912448b9151afbe9690f6dc6abc87bf22850c61
周旭 <15735559897@qq.com>
2020-04-08 23:57:35 +0800

    SimpleTests

--- src/test/java/com/ejlchina/test/SimpleTests.java
+++ src/test/java/com/ejlchina/test/SimpleTests.java
@@ -2,6 +2,7 @@ package com.ejlchina.test;
 
 import com.ejlchina.okhttps.HTTP;
 import com.ejlchina.okhttps.HttpResult;
+import com.ejlchina.okhttps.HttpResult.Body;
 import org.junit.Test;
 
 import java.util.List;
@@ -15,6 +16,7 @@ public class SimpleTests {
 
     /**
      * 同步请求示例
+     * 同步请求直接得到结果，无需设置回调
      */
     @Test
     public void testSync() {
@@ -27,6 +29,7 @@ public class SimpleTests {
 
     /**
      * 异步请求示例
+     * 异步请求在OnResponse里接收请求结果
      */
     @Test
     public void testAsync() {
@@ -38,6 +41,40 @@ public class SimpleTests {
                 })
                 .get();
         sleep(5000);
+    }
+
+
+    /**
+     * 启用 cache 示例
+     */
+    @Test
+    public void testCache() {
+        Body body = http.sync("/users").get().getBody()
+                .cache();   // 启用 cache
+        // 使用 cache 后，可以多次使用 toXXX() 方法
+        System.out.println(body.toString());
+        System.out.println(body.toJsonArray());
+        System.out.println(body.toList(User.class));
+    }
+
+
+    /**
+     * 获取文件大小示例
+     */
+    @Test
+    public void testContentLength() {
+        long t = System.currentTimeMillis();
+
+        long size = http.sync("https://download.cocos.com/CocosDashboard/v1.0.1/CocosDashboard-v1.0.1-win32-031816.exe")
+                .get().getBody()
+                .close()             // 只是想获得文件大小，不消费报文体，所以直接关闭
+                .getContentLength(); // 获得待下载文件的大小
+
+        t = System.currentTimeMillis() - t;
+
+        // 由于未消费报文体，所以本次请求不会消耗下载报文体的时间和网络流量）
+        System.out.println("耗时：" + t + " 毫秒");
+        System.out.println("size = " + (size / 1024) + " KB");
     }
 
 
