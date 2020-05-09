41a9f7951e88b3802b338bffeb171853bb96877f
周旭 <zhou.xu@ejlchina.com>
2020-05-09 15:48:21 +0800

    TestCases

---
+++ okhttps-jackson/src/test/java/com/ejlchina/okhttps/DateBean.java
@@ -0,0 +1,40 @@
+package com.ejlchina.okhttps;
+
+import java.util.Date;
+
+public class DateBean {
+
+    private int id;
+    private Date date;
+    
+    
+	public DateBean() {
+	}
+    
+	public DateBean(int id, Date date) {
+		this.id = id;
+		this.date = date;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "DateBean [id=" + id + ", date=" + date + "]";
+	}
+	
+}
