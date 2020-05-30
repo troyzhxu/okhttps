732c81b43c31a496e67cfec611107fe461957346
周旭 <15735559897@qq.com>
2020-05-30 13:32:21 +0800

    适配 maven 编译器

--- okhttps/src/main/java/com/ejlchina/okhttps/Platform.java
+++ okhttps/src/main/java/com/ejlchina/okhttps/Platform.java
@@ -7,15 +7,16 @@ public class Platform {
     /**
      * Android 的 SDK 版本，若不是 Android 平台，则为 0
      */
-    public static final int ANDROID_SDK_INT;
+    public static final int ANDROID_SDK_INT = getAndroidSdkInt();
 
-    static {
+
+    private static int getAndroidSdkInt() {
         try {
             Class<?> versionClass = Class.forName("android.os.Build$VERSION");
             Field field = versionClass.getDeclaredField("SDK_INT");
-            ANDROID_SDK_INT = field.getInt(field);
+            return field.getInt(field);
         } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException e) {
-            ANDROID_SDK_INT = 0;
+            return  0;
         }
     }
 
