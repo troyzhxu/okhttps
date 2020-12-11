15cff5974bedc7754ed3b76d2770355182e86da3
ZhouXu <zhou.xu@ejlchina.com>
2020-12-12 00:41:27 +0800

    HTTP.Builder 新增方法 addRetryPolicy、clearPreprocessors、clearMsgConvertors、clearRetryPolicys

--- okhttps/src/main/java/com/ejlchina/okhttps/HTTP.java
+++ okhttps/src/main/java/com/ejlchina/okhttps/HTTP.java
@@ -69,7 +69,6 @@ public interface HTTP {
 
     /**
      * OkHttp 的 Websocket 原生请求（该请求不经过 预处理器）
-     *
      * @param request  请求
      * @param listener 监听器
      * @return Websocket
@@ -96,7 +95,6 @@ public interface HTTP {
         return new Builder();
     }
 
-
     /**
      * Http 配置器
      *
@@ -110,7 +108,6 @@ public interface HTTP {
         void config(OkHttpClient.Builder builder);
 
     }
-
 
     class Builder {
 
@@ -146,6 +143,9 @@ public interface HTTP {
 
         private String bodyType = OkHttps.FORM;
 
+        private Map<String, RetryPolicy> retryPolicys;
+
+
         public Builder() {
             mediaTypes = new HashMap<>();
             mediaTypes.put("*", "application/octet-stream");
@@ -169,33 +169,35 @@ public interface HTTP {
             contentTypes.add("application/x-protobuf");
             preprocessors = new ArrayList<>();
             msgConvertors = new ArrayList<>();
+            retryPolicys = new HashMap<>();
         }
 
         public Builder(HttpClient hc) {
-            this.okClient = hc.okClient();
-            this.baseUrl = hc.baseUrl();
-            this.mediaTypes = hc.mediaTypes();
-            this.preprocessors = new ArrayList<>();
-            Collections.addAll(this.preprocessors, hc.preprocessors());
+            okClient = hc.okClient();
+            baseUrl = hc.baseUrl();
+            mediaTypes = hc.mediaTypes();
+            preprocessors = new ArrayList<>();
+            Collections.addAll(preprocessors, hc.preprocessors());
             TaskExecutor executor = hc.executor();
-            this.contentTypes = new ArrayList<>();
-            Collections.addAll(this.contentTypes, executor.getContentTypes());
-            this.mainExecutor = executor.getMainExecutor();
-            this.taskScheduler = executor.getTaskScheduler();
-            this.downloadListener = executor.getDownloadListener();
-            this.responseListener = executor.getResponseListener();
-            this.exceptionListener = executor.getExceptionListener();
-            this.completeListener = executor.getCompleteListener();
-            this.msgConvertors = new ArrayList<>();
-            Collections.addAll(this.msgConvertors, executor.getMsgConvertors());
-            this.preprocTimeoutTimes = hc.preprocTimeoutTimes();
-            this.charset = hc.charset();
-            this.bodyType = hc.bodyType();
+            contentTypes = new ArrayList<>();
+            Collections.addAll(contentTypes, executor.getContentTypes());
+            mainExecutor = executor.getMainExecutor();
+            taskScheduler = executor.getTaskScheduler();
+            downloadListener = executor.getDownloadListener();
+            responseListener = executor.getResponseListener();
+            exceptionListener = executor.getExceptionListener();
+            completeListener = executor.getCompleteListener();
+            msgConvertors = new ArrayList<>();
+            Collections.addAll(msgConvertors, executor.getMsgConvertors());
+            preprocTimeoutTimes = hc.preprocTimeoutTimes();
+            retryPolicys = new HashMap<>();
+            retryPolicys.putAll(hc.retryPolicys());
+            bodyType = hc.bodyType();
+            charset = hc.charset();
         }
 
         /**
          * 配置 OkHttpClient
-         *
          * @param config 配置器
          * @return Builder
          */
@@ -206,7 +208,6 @@ public interface HTTP {
 
         /**
          * 设置 baseUrl
-         *
          * @param baseUrl 全局URL前缀
          * @return Builder
          */
@@ -217,7 +218,6 @@ public interface HTTP {
 
         /**
          * 配置媒体类型
-         *
          * @param mediaTypes 媒体类型
          * @return Builder
          */
@@ -230,8 +230,7 @@ public interface HTTP {
 
         /**
          * 配置媒体类型
-         *
-         * @param key   媒体类型KEY
+         * @param key 媒体类型KEY
          * @param value 媒体类型VALUE
          * @return Builder
          */
@@ -268,7 +267,6 @@ public interface HTTP {
 
         /**
          * 设置回调执行器，例如实现切换线程功能，只对异步请求有效
-         *
          * @param executor 回调执行器
          * @return Builder
          */
@@ -280,6 +278,7 @@ public interface HTTP {
         /**
          * 配置 任务调度器，可用的调度由 {@link WebSocketTask#heatbeat(int, int) } 指定的心跳任务
          * 若不配置，则生成一个 线程容量为 1 的 ScheduledThreadPoolExecutor 调度器
+         * @since v2.3.0
          * @param scheduler 调度器
          * @return Builder
          */
@@ -290,7 +289,6 @@ public interface HTTP {
 
         /**
          * 添加可并行处理请求任务的预处理器
-         *
          * @param preprocessor 预处理器
          * @return Builder
          */
@@ -302,8 +300,7 @@ public interface HTTP {
         }
 
         /**
-         * 添加预处理器
-         *
+         * 添加串行预处理器
          * @param preprocessor 预处理器
          * @return Builder
          */
@@ -315,77 +312,92 @@ public interface HTTP {
         }
 
         /**
+         * 清空预处理器（包括串行预处理器）
+         * @since v2.5.0
+         * @return Builder
+         */
+        public Builder clearPreprocessors() {
+            preprocessors.clear();
+            return this;
+        }
+
+        /**
          * 最大预处理时间（倍数，相当普通请求的超时时间）
-         *
          * @param times 普通超时时间的倍数，默认为 10
          * @return Builder
          */
         public Builder preprocTimeoutTimes(int times) {
             if (times > 0) {
-                this.preprocTimeoutTimes = times;
+                preprocTimeoutTimes = times;
             }
             return this;
         }
 
         /**
          * 设置全局响应监听
-         *
          * @param listener 监听器
          * @return Builder
          */
         public Builder responseListener(TaskListener<HttpResult> listener) {
-            this.responseListener = listener;
+            responseListener = listener;
             return this;
         }
 
         /**
          * 设置全局异常监听
-         *
          * @param listener 监听器
          * @return Builder
          */
         public Builder exceptionListener(TaskListener<IOException> listener) {
-            this.exceptionListener = listener;
+            exceptionListener = listener;
             return this;
         }
 
         /**
          * 设置全局完成监听
-         *
          * @param listener 监听器
          * @return Builder
          */
         public Builder completeListener(TaskListener<HttpResult.State> listener) {
-            this.completeListener = listener;
+            completeListener = listener;
             return this;
         }
 
         /**
          * 设置下载监听器
-         *
          * @param listener 监听器
          * @return Builder
          */
         public Builder downloadListener(DownListener listener) {
-            this.downloadListener = listener;
+            downloadListener = listener;
             return this;
         }
 
         /**
-         * @since 2.0.0
+         * @since v2.0.0
          * 添加消息转换器
          * @param msgConvertor JSON 服务
          * @return Builder
          */
         public Builder addMsgConvertor(MsgConvertor msgConvertor) {
             if (msgConvertor != null) {
-                this.msgConvertors.add(msgConvertor);
+                msgConvertors.add(msgConvertor);
             }
             return this;
         }
 
         /**
-         * @since 2.0.0
+         * 清空消息转换器
+         * @since v2.5.0
+         * @return Builder
+         */
+        public Builder clearMsgConvertors() {
+            msgConvertors.clear();
+            return this;
+        }
+
+        /**
+         * @since v2.0.0
          * 设置默认编码格式
          * @param charset 编码
          * @return Builder
@@ -398,7 +410,7 @@ public interface HTTP {
         }
 
         /**
-         * @since 2.0.0
+         * @since v2.0.0
          * 设置默认请求体类型
          * @param bodyType 请求体类型
          * @return Builder
@@ -407,6 +419,31 @@ public interface HTTP {
             if (bodyType != null) {
                 this.bodyType = bodyType;
             }
+            return this;
+        }
+
+        /**
+         * 添加重试策略
+         * @since v2.5.0
+         * @param name 策略名
+         * @param policy 策略
+         * @return Builder
+         */
+        public Builder addRetryPolicy(String name, RetryPolicy policy) {
+            if (retryPolicys.containsKey(name)) {
+                throw new IllegalStateException("RetryPolicy[" + name + "] already exists!");
+            }
+            retryPolicys.put(name, policy);
+            return this;
+        }
+
+        /**
+         * 清空重试策略
+         * @since v2.5.0
+         * @return Builder
+         */
+        public Builder clearRetryPolicys() {
+            retryPolicys.clear();
             return this;
         }
 
@@ -439,8 +476,7 @@ public interface HTTP {
         }
 
         private boolean needCopyInterceptor(List<Interceptor> list) {
-            return mainExecutor != null && Platform.ANDROID_SDK_INT > 24
-                    && CopyInterceptor.notIn(list);
+            return mainExecutor != null && Platform.ANDROID_SDK_INT > 24 && CopyInterceptor.notIn(list);
         }
 
         public OkHttpClient okClient() {
@@ -501,6 +537,10 @@ public interface HTTP {
 
         public String bodyType() {
             return bodyType;
+        }
+
+        public Map<String, RetryPolicy> retryPolicys() {
+            return retryPolicys;
         }
 
     }
