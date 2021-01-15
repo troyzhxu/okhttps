e4ab8c33fc78d73b7f36877cf05ef6351f12fef1
ZhouXu <zhou.xu@ejlchina.com>
2021-01-15 15:29:37 +0800

    data-fastjson 实现 Map 与 List 接口

--- okhttps-datas/okhttps-data-fastjson/src/main/java/com/ejlchina/okhttps/FastjsonArray.java
+++ okhttps-datas/okhttps-data-fastjson/src/main/java/com/ejlchina/okhttps/FastjsonArray.java
@@ -3,7 +3,12 @@ package com.ejlchina.okhttps;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
-public class FastjsonArray implements Array {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class FastjsonArray implements Array, List<Object> {
 
 	private final JSONArray json;
 	
@@ -67,6 +72,112 @@ public class FastjsonArray implements Array {
 	@Override
 	public String getString(int index) {
 		return json.getString(index);
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		return json.contains(o);
+	}
+
+	@Override
+	public Iterator<Object> iterator() {
+		return json.iterator();
+	}
+
+	@Override
+	public Object[] toArray() {
+		return json.toArray();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		//noinspection SuspiciousToArrayCall
+		return json.toArray(a);
+	}
+
+	@Override
+	public boolean add(Object o) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		return json.containsAll(c);
+	}
+
+	@Override
+	public boolean addAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void clear() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object get(int index) {
+		return json.get(index);
+	}
+
+	@Override
+	public Object set(int index, Object element) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void add(int index, Object element) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object remove(int index) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int indexOf(Object o) {
+		return json.indexOf(o);
+	}
+
+	@Override
+	public int lastIndexOf(Object o) {
+		return json.lastIndexOf(o);
+	}
+
+	@Override
+	public ListIterator<Object> listIterator() {
+		return json.listIterator();
+	}
+
+	@Override
+	public ListIterator<Object> listIterator(int index) {
+		return json.listIterator(index);
+	}
+
+	@Override
+	public List<Object> subList(int fromIndex, int toIndex) {
+		return json.subList(fromIndex, toIndex);
 	}
 
 	@Override
