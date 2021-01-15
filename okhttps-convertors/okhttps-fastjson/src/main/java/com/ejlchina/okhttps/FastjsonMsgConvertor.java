be76513ad9164b3906326925ff265404c894afe3
ZhouXu <zhou.xu@ejlchina.com>
2021-01-15 12:54:24 +0800

    FastjsonDataConvertor

--- okhttps-convertors/okhttps-fastjson/src/main/java/com/ejlchina/okhttps/FastjsonMsgConvertor.java
+++ okhttps-convertors/okhttps-fastjson/src/main/java/com/ejlchina/okhttps/FastjsonMsgConvertor.java
@@ -8,28 +8,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Type;
 import java.nio.charset.Charset;
-import java.util.List;
 
-public class FastjsonMsgConvertor implements MsgConvertor, ConvertProvider {
+public class FastjsonMsgConvertor extends FastjsonDataConvertor implements MsgConvertor, ConvertProvider {
 
 	@Override
 	public String mediaType() {
 		return "application/json";
-	}
-
-	@Override
-	public Mapper toMapper(InputStream in, Charset charset) {
-		return new FastjsonMapper(JSON.parseObject(toString(in, charset)));
-	}
-
-	@Override
-	public Array toArray(InputStream in, Charset charset) {
-		return new FastjsonArray(JSON.parseArray(toString(in, charset)));
-	}
-
-	@Override
-	public byte[] serialize(Object object, Charset charset) {
-		return JSON.toJSONString(object).getBytes(charset);
 	}
 
 	@Override
@@ -38,16 +22,12 @@ public class FastjsonMsgConvertor implements MsgConvertor, ConvertProvider {
 	}
 
 	@Override
-	public <T> List<T> toList(Class<T> type, InputStream in, Charset charset) {
-		return JSON.parseArray(toString(in, charset), type);
-	}
-
-	@Override
 	public MsgConvertor getConvertor() {
 		return new FastjsonMsgConvertor();
 	}
 
-	private String toString(InputStream in, Charset charset) {
+	@Override
+	protected String toString(InputStream in, Charset charset) {
 		try {
 			return Okio.buffer(Okio.source(in)).readString(charset);
 		} catch (IOException e) {
