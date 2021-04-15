c46dd23a99bd2ed3a62424d77ffedf0ae4e8a07d
ZhouXu <zhou.xu@ejlchina.com>
2021-04-16 00:07:57 +0800

    注释

--- okhttps/src/main/java/com/ejlchina/okhttps/OkHttps.java
+++ okhttps/src/main/java/com/ejlchina/okhttps/OkHttps.java
@@ -12,7 +12,9 @@ import okhttp3.WebSocketListener;
  */
 public final class OkHttps {
 
+    // 使用 x-www-form-urlencoded 表单格式
     public static final String FORM = "form";
+    // 使用 form-data 表单格式（一般上传文件时使用）
     public static final String FORM_DATA = "form-data";
     public static final String JSON = "json";
     public static final String XML = "xml";
