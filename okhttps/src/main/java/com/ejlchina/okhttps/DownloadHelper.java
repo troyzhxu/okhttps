5545c6ab5f485dd07c1b2da6a80878e74dc3f104
zhouxu <zhou.xu@ejlchina.com>
2022-01-17 16:13:06 +0800

    优化下载文件名生成逻辑，再未指定文件名的情况下更据 Content-Type 自动生成文件的扩展名

--- okhttps/src/main/java/com/ejlchina/okhttps/DownloadHelper.java
+++ okhttps/src/main/java/com/ejlchina/okhttps/DownloadHelper.java
@@ -1,13 +1,27 @@
 package com.ejlchina.okhttps;
 
+import okhttp3.MediaType;
+
 import java.io.File;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 下载助手，用户解析下载路径与文件名
  */
 public class DownloadHelper {
+
+    private Map<String, String> extMappings = new HashMap<>();
+
+    public DownloadHelper() {
+        extMappings.put("video/mpeg4", "mp4");
+        extMappings.put("text/plain", "txt");
+        extMappings.put("application/x-xls", "text/xls");
+        extMappings.put("application/vnd.android.package-archive", "apk");
+        extMappings.put("application/msword", "doc");
+    }
 
     /**
      * 解析下载文件名
@@ -15,18 +29,35 @@ public class DownloadHelper {
      * @return 文件名
      */
     public String resolveFileName(HttpResult result) {
-        String fileName = result.getHeader("Content-Disposition");
+        String contentDisposition = result.getHeader("Content-Disposition");
         // 通过 Content-Disposition 获取文件名，这点跟服务器有关，需要灵活变通
-        if (fileName == null || fileName.length() < 1) {
-            fileName = result.getTask().getUrl();
-            fileName = fileName.substring(fileName.lastIndexOf("/") + 1);
+        if (contentDisposition == null || contentDisposition.length() < 1) {
+            String urlPath = result.getTask().getUrl();
+            String urlName = urlPath.substring(urlPath.lastIndexOf("/") + 1);
+            return toFileName(urlName, result);
         } else {
-            fileName = URLDecoder.decode(fileName.substring(
-                    fileName.indexOf("filename=") + 9), StandardCharsets.UTF_8);
+            String filename = URLDecoder.decode(contentDisposition.substring(
+                    contentDisposition.indexOf("filename=") + 9), StandardCharsets.UTF_8);
             // 有些文件名会被包含在""里面，所以要去掉，不然无法读取文件后缀
-            fileName = fileName.replaceAll("\"", "");
+            return filename.replaceAll("\"", "");
         }
-        return fileName;
+    }
+
+    protected String toFileName(String name, HttpResult result) {
+        int dotIndex = name.lastIndexOf('.');
+        if (dotIndex > -1 && dotIndex < name.length() - 1) {
+            return name;
+        }
+        MediaType type = result.getBody().getType();
+        // 获取文件扩展名
+        String ext = extMappings.get(type.toString());
+        if (ext == null) {
+            ext = type.subtype();
+        }
+        if (dotIndex == -1) {
+            return name + "." + ext;
+        }
+        return name + ext;
     }
 
     /**
@@ -59,6 +90,14 @@ public class DownloadHelper {
             return name + "(" + index + ")" + ext;
         }
         return "(" + index + ")" + ext;
+    }
+
+    public Map<String, String> getExtMappings() {
+        return extMappings;
+    }
+
+    public void setExtMappings(Map<String, String> extMappings) {
+        this.extMappings = extMappings;
     }
 
 }
