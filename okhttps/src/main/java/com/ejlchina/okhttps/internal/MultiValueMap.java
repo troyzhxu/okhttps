816a696188d0b027e08d1e230bf2d858b646a586
zhouxu <zhou.xu@ejlchina.com>
2022-04-06 10:29:08 +0800

    优化 MultiValueMap

--- okhttps/src/main/java/com/ejlchina/okhttps/internal/MultiValueMap.java
+++ okhttps/src/main/java/com/ejlchina/okhttps/internal/MultiValueMap.java
@@ -3,30 +3,28 @@ package com.ejlchina.okhttps.internal;
 import java.util.*;
 
 /**
+ * 一个 Key 可以有多个 Value 的 有序 Map 集合
  * @author troy zhou
  * @since v3.5.0
- * @param <K> 泛型 键
  * @param <V> 泛型 值
  */
-public class MultiValueMap<K, V> extends AbstractMap<K, V> {
+public class MultiValueMap<V> extends AbstractMap<String, V> {
 
-    private final List<K> names = new ArrayList<>();
-    private final List<V> values = new ArrayList<>();
 
-    @Override
-    public V put(K key, V value) {
-        names.add(key);
-        values.add(value);
-        return null;
-    }
+    transient final List<String> keys = new ArrayList<>();
 
-    Set<Entry<K, V>> entrySet;
+    transient final List<V> values = new ArrayList<>();
 
-    class EntrySet extends AbstractSet<Entry<K, V>> {
+    transient Set<Entry<String, V>> entrySet;
+
+    /**
+     * 键值对集合类
+     */
+    class EntrySet extends AbstractSet<Entry<String, V>> {
 
         @Override
-        public Iterator<Entry<K, V>> iterator() {
-            Iterator<K> nit = names.iterator();
+        public Iterator<Entry<String, V>> iterator() {
+            Iterator<String> nit = keys.iterator();
             Iterator<V> vit = values.iterator();
             return new Iterator<>() {
 
@@ -36,7 +34,7 @@ public class MultiValueMap<K, V> extends AbstractMap<K, V> {
                 }
 
                 @Override
-                public Entry<K, V> next() {
+                public Entry<String, V> next() {
                     return new SimpleEntry<>(nit.next(), vit.next());
                 }
 
@@ -50,15 +48,68 @@ public class MultiValueMap<K, V> extends AbstractMap<K, V> {
 
         @Override
         public int size() {
-            return names.size();
+            return keys.size();
         }
 
     }
 
+    /**
+     * @return 键值对集合
+     */
     @Override
-    public Set<Entry<K, V>> entrySet() {
-        Set<Map.Entry<K,V>> es = entrySet;
+    public Set<Entry<String, V>> entrySet() {
+        Set<Map.Entry<String,V>> es = entrySet;
         return es == null ? (entrySet = new EntrySet()) : es;
+    }
+
+    /**
+     * 向 Map 里放值
+     * @param key 键
+     * @param value 值
+     * @return always null
+     */
+    @Override
+    public V put(String key, V value) {
+        // 只存放非空值
+        if (key != null && value != null) {
+            keys.add(key);
+            values.add(value);
+        }
+        return null;
+    }
+
+    /**
+     * 获取 Key 对应的最后（新）的一个值
+     * @param key 键
+     * @return 最后（新）的一个值
+     */
+    @Override
+    public V get(Object key) {
+        if (key instanceof String) {
+            for (int i = keys.size() - 1; i >= 0; i--) {
+                if (keys.get(i).equals(key)) {
+                    return values.get(i);
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 获取 Key 下的所有 Value
+     * @param key 键
+     * @return List<V>
+     */
+    public List<V> getValues(String key) {
+        List<V> list = new ArrayList<>();
+        if (key != null) {
+            for (int i = 0; i < keys.size(); i++) {
+                if (keys.get(i).equals(key)) {
+                    list.add(values.get(i));
+                }
+            }
+        }
+        return list;
     }
 
 }
--- okhttps/src/test/java/com/ejlchina/test/MultiValueMapTests.java
+++ okhttps/src/test/java/com/ejlchina/test/MultiValueMapTests.java
@@ -2,34 +2,74 @@ package com.ejlchina.test;
 
 import com.ejlchina.okhttps.Platform;
 import com.ejlchina.okhttps.internal.MultiValueMap;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MultiValueMapTests {
 
+    @Test
+    public void test1() {
+        Map<String, String> m = new HashMap<>();
+        m.put("sex", "male");
+        m.put("school", "School");
+
+        Map<String, String> map = new MultiValueMap<>();
+        map.putAll(m);
+
+        Assert.assertEquals("male", map.get("sex"));
+        Assert.assertEquals("School", map.get("school"));
+        Assert.assertEquals(2, map.size());
+    }
 
     @Test
     public void test() {
-        Map<String, String> m = new HashMap<>();
-        m.put("sex", "male");
-        m.put("school", "High School");
-
-        Map<String, String> map = new MultiValueMap<>();
-
+        MultiValueMap<Object> map = new MultiValueMap<>();
         map.put("name", "Jack");
-        map.putAll(m);
+        map.put("age", 25);
         map.put("name", "Tom");
-        map.put("age", "16");
+        map.put("age", 30);
         map.put("name", "Alice");
+        map.put("age", 26);
 
-        map.putIfAbsent("age1", "17");
+        Assert.assertEquals(6, map.size());
+        Assert.assertEquals("Alice", map.get("name"));
+        Assert.assertEquals(26, map.get("age"));
 
-        map.clear();
+        String[] NAMES = new String[] {
+                "Jack", "Tom", "Alice"
+        };
+        int[] AGES = new int[] {
+                25, 30, 26
+        };
+
+        List<Object> names = map.getValues("name");
+        Assert.assertEquals(3, names.size());
+        Assert.assertEquals(NAMES[0], names.get(0));
+        Assert.assertEquals(NAMES[1], names.get(1));
+        Assert.assertEquals(NAMES[2], names.get(2));
+
+        List<Object> ages = map.getValues("age");
+        Assert.assertEquals(3, ages.size());
+        Assert.assertEquals(AGES[0], ages.get(0));
+        Assert.assertEquals(AGES[1], ages.get(1));
+        Assert.assertEquals(AGES[2], ages.get(2));
+
+        AtomicInteger index = new AtomicInteger(0);
 
         Platform.forEach(map, (key, value) -> {
-            System.out.println(key + " = " + value);
+            int i = index.getAndIncrement();
+            if (i % 2 == 1) {
+                Assert.assertEquals("age", key);
+                Assert.assertEquals(AGES[i / 2], value);
+            } else {
+                Assert.assertEquals("name", key);
+                Assert.assertEquals(NAMES[i / 2], value);
+            }
         });
     }
 
